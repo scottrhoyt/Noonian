868b4e19d768e3c6e5ab8a5d043053be8264d6b6
Scott Hoyt <scott@wildonion.io>
2016-10-31 19:09:36 -0700

    Add stub test.

---
+++ noonianTests/NothingTests.swift
@@ -0,0 +1,14 @@
+//
+//  NothingTests.swift
+//  noonian
+//
+//  Created by Scott Hoyt on 10/31/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+import XCTest
+@testable import noonian
+
+class NothingTests: XCTestCase {
+    func testAlwaysPasses() { }
+}
