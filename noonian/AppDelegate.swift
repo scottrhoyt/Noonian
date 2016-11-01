68d5d00da4b897c979b2af338379c83e2ac7c770
Scott Hoyt <scott@wildonion.io>
2016-10-31 18:13:21 -0700

    Initial Commit

---
+++ noonian/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  noonian
+//
+//  Created by Scott Hoyt on 10/31/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
