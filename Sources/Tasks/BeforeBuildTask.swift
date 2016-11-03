812e0f7082f5d4ebc529e32937e3df067c62bea7
Scott Hoyt <scott@wildonion.io>
2016-11-02 23:41:40 -0700

    Stub out Task protocol and BeforeBuildTask.

---
+++ Sources/Tasks/BeforeBuildTask.swift
@@ -0,0 +1,15 @@
+//
+//  BeforeBuildTask.swift
+//  Noonian
+//
+//  Created by Scott Hoyt on 11/2/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+import Foundation
+
+struct BeforeBuildTask: Task {
+    func run() {
+        fatalError()
+    }
+}
---
+++ Sources/Tasks/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  Noonian
+//
+//  Created by Scott Hoyt on 11/2/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+import Foundation
+
+protocol Task {
+    func run()
+}
